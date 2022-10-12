c65c082621b2100dbe9532e3681c636c5fddd9ef
Omar Iltaf <omariltaf@gmail.com>
2022-10-12 21:49:21 +0100

    Determine current time in human format via cli

---
+++ src/cli/cli.ts
@@ -0,0 +1,8 @@
+import { TalkingClock } from "../clock/clock";
+
+// remember to validate inputs for obj 2/3
+
+const talkingClock = new TalkingClock();
+const humanTime = talkingClock.getHumanTime();
+
+console.log(humanTime);
---
+++ src/clock/clock.ts
@@ -0,0 +1,59 @@
+import converter from "number-to-words";
+
+export class TalkingClock {
+	getHumanTime(): string {
+		try {
+			const { hour, minute } = this.getCurrentHourAndMinute();
+			return this.determineHumanTime(hour, minute);
+		} catch (error: any) {
+			return `Error: ${error.message}`;
+		}
+	}
+
+	private getCurrentHourAndMinute(): { hour: number; minute: number } {
+		const currentDate = new Date();
+		return {
+			hour: currentDate.getHours(),
+			minute: currentDate.getMinutes(),
+		};
+	}
+
+	private determineHumanTime(hour: number, minute: number): string {
+		let humanTime = "";
+		const convertedHour = this.convertToTwelveHourTime(hour);
+		if (minute === 0) {
+			humanTime += `${this.numberToWord(convertedHour)} o'clock`;
+		} else if (minute < 30) {
+			humanTime += `${this.numberToWord(minute)} past ${this.numberToWord(
+				convertedHour
+			)}`;
+		} else if (minute === 30) {
+			humanTime += `half past ${this.numberToWord(convertedHour)}`;
+		} else {
+			const convertedNextHour = this.convertToTwelveHourTime(hour + 1);
+			const minutesToNextHour = 60 - minute;
+			humanTime += `${this.numberToWord(
+				minutesToNextHour
+			)} to ${this.numberToWord(convertedNextHour)}`;
+		}
+
+		if (!humanTime) {
+			throw new Error("Human Time cannot be determined");
+		}
+
+		return this.capitaliseFirstLetter(humanTime);
+	}
+
+	private convertToTwelveHourTime(hour: number) {
+		const modulo = hour % 12;
+		return modulo === 0 ? 12 : modulo;
+	}
+
+	private numberToWord(number: number): string {
+		return converter.toWords(number);
+	}
+
+	private capitaliseFirstLetter(text: string): string {
+		return text.charAt(0).toUpperCase() + text.slice(1);
+	}
+}
